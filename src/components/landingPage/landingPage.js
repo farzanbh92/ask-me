97a083550e7b01e2898db64e937ec3597629bc10
Farzan <farzan.behrooznia@gmail.com>
2023-08-05 22:59:43 +0330

    uploading

---
+++ src/components/landingPage/landingPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <>
+       <div className='flex flex-col md:flex-row'>
+        <div className='w-full md:w-1/5 md:order-2 md:h-screen bg-gray-900 p-2'>
+            <div className=''>
+               
+            </div>
+        </div>
+        <div className='w-full md:w-4/5 md:order-1 md:h-screen'>b</div>
+       </div>
+        </>
+    );
+};
+
+export default LandingPage;
