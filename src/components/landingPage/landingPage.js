f38046f529cdc7cbcac6beecf3adfef9044ab447
farzanbh92 <farzan.behrooznia@gmail.com>
2023-08-06 11:40:41 +0330

    3rd commit

--- src/components/landingPage/landingPage.js
+++ src/components/landingPage/landingPage.js
@@ -5,12 +5,22 @@ const LandingPage = () => {
     return (
         <>
        <div className='flex flex-col md:flex-row'>
-        <div className='w-full md:w-1/5 md:order-2 md:h-screen bg-gray-900 p-2'>
-            <div className='grid grid-cols-3 gap-2 h-20 bg-yellow-200 p-4'>
-             <div className='border-solid border-white bg-green-200 rounded-lg pt-2 h-12'>sidetoggle</div>
-             <div className='col-span-2 border-solid border-white bg-red-300 pt-2 h-12'>چت جدید
-             <img src={plus} alt='add' className='w-8'/>
-             چت جدید
+        <div className='w-full md:w-1/5 md:order-2 md:h-screen bg-gray-900'>
+
+            <div className='grid grid-cols-3 gap-2 h-20 content-center mx-2'>
+
+            <div className='border-2 border-white rounded-lg pt-2 h-12'>
+               <p className='text-white cursor-pointer'>sidetoggle</p> 
+            </div>
+
+             <div className='col-span-2 border-2 border-white  rounded-lg h-12'> 
+
+             <div className='grid grid-cols-2 items-center cursor-pointer'>
+           
+             <div className='mx-auto text-white'>چت جدید</div>
+             <div className='mx-auto text-white text-3xl'>+</div>
+             </div>
+             
              </div>
              
             </div>
