be8576d20b6040ecadd32e5cf2819f669e88bd1b
farzanbh92 <farzan.behrooznia@gmail.com>
2023-08-06 08:09:57 +0330

    editing sidebar

--- src/components/landingPage/landingPage.js
+++ src/components/landingPage/landingPage.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import plus from '../../assets/images/plus.jpg';
 
 const LandingPage = () => {
     return (
         <>
        <div className='flex flex-col md:flex-row'>
         <div className='w-full md:w-1/5 md:order-2 md:h-screen bg-gray-900 p-2'>
-            <div className=''>
-               
+            <div className='grid grid-cols-3 gap-2 h-20 bg-yellow-200 p-4'>
+             <div className='border-solid border-white bg-green-200 rounded-lg pt-2 h-12'>sidetoggle</div>
+             <div className='col-span-2 border-solid border-white bg-red-300 pt-2 h-12'>چت جدید
+             <img src={plus} alt='add' className='w-8'/>
+             چت جدید
+             </div>
+             
             </div>
+           
         </div>
         <div className='w-full md:w-4/5 md:order-1 md:h-screen'>b</div>
        </div>
