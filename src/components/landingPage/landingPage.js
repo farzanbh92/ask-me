c4c9594f7358535742dce0531aae4cf5d68a0a4f
Farzan <farzan.behrooznia@gmail.com>
2023-08-06 22:57:44 +0330

    adding content section

--- src/components/landingPage/landingPage.js
+++ src/components/landingPage/landingPage.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import plus from '../../assets/images/plus.jpg';
+import sidetoggle from '../../assets/images/sidebar toggle.png';
 
 const LandingPage = () => {
+
+
     return (
         <>
-       <div className='flex flex-col md:flex-row'>
-        <div className='w-full md:w-1/5 md:order-2 md:h-screen bg-gray-900'>
+    <div className='flex flex-col md:flex-row'>
 
-            <div className='grid grid-cols-3 gap-2 h-20 content-center mx-2'>
+{/*sidebar*/}
 
-            <div className='border-2 border-white rounded-lg pt-2 h-12'>
-               <p className='text-white cursor-pointer'>sidetoggle</p> 
+        <div className='w-full md:w-1/5 md:order-2  bg-gray-900'>
+
+            <div className='grid grid-cols-3 gap-2 h-16 content-center mx-2'>
+             
+            <div className='border border-gray-100 rounded-lg pt-1 h-12'>
+           <img src={sidetoggle} alt='' className='w-8 mx-auto' />
             </div>
 
-             <div className='col-span-2 border-2 border-white  rounded-lg h-12'> 
+             <div className='col-span-2 border border-gray-100 rounded-lg h-12'> 
 
              <div className='grid grid-cols-2 items-center cursor-pointer'>
            
@@ -26,8 +31,59 @@ const LandingPage = () => {
             </div>
            
         </div>
-        <div className='w-full md:w-4/5 md:order-1 md:h-screen'>b</div>
-       </div>
+
+{/*content*/}
+
+        <div className='w-full md:w-4/5 md:order-1'>
+
+           <div className='grid grid-cols-1 w-full'>
+
+            <div className='grid grid-cols-1 content-center w-full h-28 md:h-52'>
+
+             <div className='text-3xl font-bold'>از من بپرس</div>
+
+            </div>
+
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-x-4 pt-20 md:px-28 px-8'>
+
+             <div className='grid grid-cols-1 gap-y-4 mb-8'>
+
+             <div className='p-4'>مثال ها</div>
+
+                <div className='bg-gray-200 p-4 rounded-lg cursor-pointer hover:bg-gray-300'>مثال 1</div>
+                <div className='bg-gray-200 p-4 rounded-lg cursor-pointer hover:bg-gray-300'>مثال 2</div>
+                <div className='bg-gray-200 p-4 rounded-lg cursor-pointer hover:bg-gray-300'>مثال 3</div>
+               
+             </div>
+             
+              <div className='grid grid-cols-1 gap-y-4 mb-8'>
+
+             <div className='p-4'>توانایی ها </div>
+
+                <div className='bg-gray-200 p-4 rounded-lg cursor-pointer hover:bg-gray-300'>توانایی 1</div>
+                <div className='bg-gray-200 p-4 rounded-lg cursor-pointer hover:bg-gray-300'>توانایی 2</div>
+                <div className='bg-gray-200 p-4 rounded-lg cursor-pointer hover:bg-gray-300'>توانایی 3</div>
+               
+             </div>
+
+              <div className='grid grid-cols-1 gap-y-4 mb-8'>
+
+             <div className='p-4'>محدودیت ها </div>
+
+                <div className='bg-gray-200 p-4 rounded-lg cursor-pointer hover:bg-gray-300'>محدودیت 1</div>
+                <div className='bg-gray-200 p-4 rounded-lg cursor-pointer hover:bg-gray-300'>محدودیت 2</div>
+                <div className='bg-gray-200 p-4 rounded-lg cursor-pointer hover:bg-gray-300'>محدودیت 3</div>
+               
+             </div>
+             
+             
+            </div>
+
+           </div>
+
+        </div>
+
+    </div>
         </>
     );
 };
